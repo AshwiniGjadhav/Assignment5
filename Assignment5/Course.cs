3e754fbcd3381bb7f1bc2581b98190140f9b082a
Ashwini <Ashwini@LAPTOP-FQL7UM83>
2022-09-21 17:06:36 +0530

    Add project files.

---
+++ Assignment5/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    public class Course
+    {
+       // class Course with course id , name, fees.create an array of courses & display the list
+
+            public int Id{get;set;}
+             public string Name { get;set;}
+
+            public int Fees { get;set;}
+
+            public override string ToString()
+        {
+            return $"Course Details: {Id} {Name} {Fees}";
+        }
+
+    }
+}
---
+++ Assignment5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course[] course =
+            {
+           new Course {Id= 1, Name = "MCA", Fees = 340000 },
+            new Course  { Id = 2,Name = "BBA",Fees = 25000},
+         new Course  { Id = 4,Name = "BCA",Fees = 45000},
+            };
+
+            foreach (Course c in course) 
+            {
+                Console.WriteLine(c);
+            }
+
+        }
+    }
+}
